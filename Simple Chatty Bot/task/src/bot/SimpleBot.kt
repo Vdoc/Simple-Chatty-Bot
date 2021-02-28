a4d51500c42e26e0a3bf73db6886280a35f5a618
NikoBolt <docnikbolt@gmail.com>
2021-02-28 21:09:56 +0300

    Stage 5/5: Multiple choice

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -2,8 +2,74 @@ package bot
 
 import java.util.Scanner
 /*
-Stage 4/5: Learning numbers
+Stage 5/5: Multiple choice
 */
+val scanner = Scanner(System.`in`) // Do not change this line
+
+fun main() {
+    greet("Aid", "2020") // change it as you need
+    remindName()
+    guessAge()
+    count()
+    // ...
+    test()
+    // ...
+    end()
+}
+
+fun greet(assistantName: String, birthYear: String) {
+    println("Hello! My name is ${assistantName}.")
+    println("I was created in ${birthYear}.")
+    println("Please, remind me your name.")
+}
+
+fun remindName() {
+    val name = scanner.nextLine()
+    println("What a great name you have, ${name}!")
+}
+
+fun guessAge() {
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+    println("Your age is ${age}; that's a good time to start programming!")
+}
+
+fun count() {
+    println("Now I will prove to you that I can count to any number you want.")
+    val num = scanner.nextInt()
+    for (i in 0..num) {
+        print(i)
+        println("!")
+    }
+}
+
+fun test() {
+    println("Let's test your programming knowledge.")
+    // write your code here
+    println("Why do we use methods?\n" +
+            "1. To repeat a statement multiple times.\n" +
+            "2. To decompose a program into several small subroutines.\n" +
+            "3. To determine the execution time of a program.\n" +
+            "4. To interrupt the execution of a program.")
+    while (scanner.nextInt() != 2) {
+        println("Please, try again.")
+    }
+}
+
+fun end() {
+    println("Congratulations, have a nice day!") // Do not change this text
+}
+
+
+
+
+/*
+Stage 4/5: Learning numbers
+
 fun main() {
     val scanner = Scanner(System.`in`)
 
@@ -33,31 +99,26 @@ fun main() {
 
     println("Completed, have a nice day!")
 }
+*/
 
 
 
 /*
 Stage 3/5: Guess the age
-
 fun main() {
     val scanner = Scanner(System.`in`)
-
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
     println("Please, remind me your name.")
-
     val name = scanner.nextLine()
-
     println("What a great name you have, " + name + "!")
     println("Let me guess your age.")
     println("Enter remainders of dividing your age by 3, 5 and 7.")
-
     // reading all remainders
     val remainder3 = scanner.nextInt()
     val remainder5 = scanner.nextInt()
     val remainder7 = scanner.nextInt()
     val yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
-
     println("Your age is $yourAge; that's a good time to start programming!")
 }
 */
