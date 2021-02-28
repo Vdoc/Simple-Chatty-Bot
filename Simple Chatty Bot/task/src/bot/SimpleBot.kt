010332efb15b71eedfb7234c606f39054e4309f4
NikoBolt <docnikbolt@gmail.com>
2021-02-28 20:52:36 +0300

    Stage 4/5: Learning numbers

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -2,8 +2,43 @@ package bot
 
 import java.util.Scanner
 /*
-Stage 3/5: Guess the age
+Stage 4/5: Learning numbers
 */
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Hello! My name is Aid.")
+    println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    val name = scanner.nextLine()
+
+    println("What a great name you have, " + name + "!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    val rem3 = scanner.nextInt()
+    val rem5 = scanner.nextInt()
+    val rem7 = scanner.nextInt()
+    val age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    println("Your age is " + age + "; that's a good time to start programming!")
+    println("Now I will prove to you that I can count to any number you want.")
+
+    // read a number and count to it here
+    val num = scanner.nextInt()
+    for (i in 0..num) {
+        println("$i!")
+    }
+
+    println("Completed, have a nice day!")
+}
+
+
+
+/*
+Stage 3/5: Guess the age
+
 fun main() {
     val scanner = Scanner(System.`in`)
 
@@ -25,22 +60,18 @@ fun main() {
 
     println("Your age is $yourAge; that's a good time to start programming!")
 }
-
+*/
 
 
 /*
 Stage 2/5: Print your name
-
 fun main() {
     val scanner = Scanner(System.`in`)
-
     println("Hello! My name is Aid.")
     println("I was created in 2020.")
     println("Please, remind me your name.")
-
     // reading a name
     val yourName = scanner.next()
-
     println("What a great name you have, $yourName!")
 }
 */
