d926e9ccb62faa6de1b4bc7708e09339b9598611
NikoBolt <docnikbolt@gmail.com>
2021-02-28 20:40:39 +0300

    Stage 3/5: Guess the age

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -2,8 +2,35 @@ package bot
 
 import java.util.Scanner
 /*
-Stage 2/5: Print your name
+Stage 3/5: Guess the age
 */
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("Hello! My name is Aid.")
+    println("I was created in 2020.")
+    println("Please, remind me your name.")
+
+    val name = scanner.nextLine()
+
+    println("What a great name you have, " + name + "!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    // reading all remainders
+    val remainder3 = scanner.nextInt()
+    val remainder5 = scanner.nextInt()
+    val remainder7 = scanner.nextInt()
+    val yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+    println("Your age is $yourAge; that's a good time to start programming!")
+}
+
+
+
+/*
+Stage 2/5: Print your name
+
 fun main() {
     val scanner = Scanner(System.`in`)
 
@@ -16,10 +43,13 @@ fun main() {
 
     println("What a great name you have, $yourName!")
 }
+*/
+
+
+
 
 /*
 Stage 1/5: Chatty Bot welcomes you
-
 fun main() {
     // write your code here
     val name = "Fon"
