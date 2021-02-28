49e599212c325a0e06bce19090638d56c3ebb19d
NikoBolt <docnikbolt@gmail.com>
2021-02-28 20:10:49 +0300

    init comit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -0,0 +1,9 @@
+package bot
+
+fun main() {
+    // write your code here
+    val name = "Fon"
+    val bornDate = "2021"
+    print("Hello! My name is $name.\n" +
+            "I was created in $bornDate.")
+}
